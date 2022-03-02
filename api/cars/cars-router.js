0506c7a2126b034ae6d084c6d1d543ecac4bce13
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2022-03-01 19:35:00 -0600

    working on router, done with model

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,2 +1,9 @@
 const express = require('express')
-const knex = require('knex')
+
+const carsModel = require('./cars-model')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    console.log('this is routers')
+})
